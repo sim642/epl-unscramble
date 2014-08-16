235aeb160fb3b691dfc4ee618f030efd49888f79
Simmo Saan <simmo.saan@gmail.com>
2014-08-16 22:27:10 +0300

    Add user specific data to GA events

--- web.js
+++ web.js
@@ -11,25 +11,25 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(ua.middleware('UA-53893556-1', {cookieName: '_ga'}));
 
 app.get('/', function(req, res) {
-	req.visitor.pageview('/').send();
+	req.visitor.pageview({dp: '/', uip: req.ip, ua: req.headers['user-agent']}).send();
 
 	res.redirect('https://github.com/sim642/epl-unscramble');
 });
 
 app.get('/browser.js', function(req, res) {
-	req.visitor.pageview('/browser.js').send();
+	req.visitor.pageview({dp: '/browser.js', uip: req.ip, ua: req.headers['user-agent']}).send();
 
 	res.sendFile(__dirname + '/browser.js');
 });
 
 app.post('/unscramble', function(req, res) {
-	req.visitor.pageview('/unscramble').event('Processing', 'unscramble', req.body.url).send();
+	req.visitor.pageview({dp: '/unscramble', uip: req.ip, ua: req.headers['user-agent']}).event({ec: 'Processing', ea: 'unscramble', el: req.body.url, uip: req.ip, ua: req.headers['user-agent']}).send();
 	var time = process.hrtime();
 
 	unscrambler.unscramble(req.body.text, req.body.extra, function(text) {
 		var diff = process.hrtime(time);
 		var ms = Math.round(diff[0] * 1e3 + diff[1] / 1e6);
-		req.visitor.timing('Processing', 'unscramble', ms, req.body.url).send();
+		req.visitor.timing({utc: 'Processing', etv: 'unscramble', ett: ms, utl: req.body.url, uip: req.ip, ua: req.headers['user-agent']}).send();
 
 		res.set({
 			'Access-Control-Allow-Origin': '*'
