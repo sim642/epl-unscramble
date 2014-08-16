912541f59adbd43ef1f44c58e9874b4c8ebaac1e
Simmo Saan <simmo.saan@gmail.com>
2014-08-16 21:59:17 +0300

    Add Google Analytics tracking

--- browser.js
+++ browser.js
@@ -1,7 +1,8 @@
 $.post("http://epl-unscramble.herokuapp.com/unscramble", {
 	text: $('div.obfuscated_body').html(),
 	extra: $('font.articleBody').clone().children().remove().end().text().trim() +
-		" " + $('font.imgCapS').text()
+		" " + $('font.imgCapS').text(),
+	url: document.URL
 }, function(data) {
 	$('div.obfuscated_body').html(data).removeClass('obfuscated_body');
 	$('.sso-art-wrapper').hide();
--- web.js
+++ web.js
@@ -1,19 +1,36 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var logfmt = require('logfmt');
+var ua = require('universal-analytics');
 
 var app = express();
 var unscrambler = require('./unscrambler');
 
 app.use(logfmt.requestLogger());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(ua.middleware('UA-53893556-1', {cookieName: '_ga'}));
+
+app.get('/', function(req, res) {
+	req.visitor.pageview('/').send();
+
+	res.redirect('https://github.com/sim642/epl-unscramble');
+});
 
 app.get('/browser.js', function(req, res) {
+	req.visitor.pageview('/browser.js').send();
+
 	res.sendFile(__dirname + '/browser.js');
 });
 
 app.post('/unscramble', function(req, res) {
+	req.visitor.pageview('/unscramble').event('Processing', 'unscramble', req.body.url).send();
+	var time = process.hrtime();
+
 	unscrambler.unscramble(req.body.text, req.body.extra, function(text) {
+		var diff = process.hrtime(time);
+		var ms = Math.round(diff[0] * 1e3 + diff[1] / 1e6);
+		req.visitor.timing('Processing', 'unscramble', ms, req.body.url).send();
+
 		res.set({
 			'Access-Control-Allow-Origin': '*'
 		});
