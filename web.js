20eee5601baa1112f4f1b895322c084427c241fc
Simmo Saan <simmo.saan@gmail.com>
2015-02-02 18:39:56 +0200

    Use express's proxy IP handling

--- web.js
+++ web.js
@@ -10,34 +10,15 @@ var util = require('util');
 var app = express();
 var unscrambler = require('./unscrambler');
 
+app.enable('trust proxy');
+
 app.use(logfmt.requestLogger());
 app.use(bodyParser.urlencoded({extended: false})); // POST body parser
 app.use(cookieParser()); // parse cookies to be used with GA
 app.use(session({secret: '7f52d94ac59a47c352ffaab4e6515730240546c2'})); // allow setting cookies
 app.use(ua.middleware('UA-53893556-1', {cookieName: '_ga'})); // Google Analytics
 
-// https://stackoverflow.com/questions/14382725/how-to-get-the-correct-ip-address-of-a-client-into-a-node-socket-io-app-hosted-o
-function getClientIp(req) {
-	var ipAddress;
-	// Amazon EC2 / Heroku workaround to get real client IP
-	var forwardedIpsStr = req.header('x-forwarded-for'); 
-	if (forwardedIpsStr) {
-		// 'x-forwarded-for' header may return multiple IP addresses in
-		// the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
-		// the first one
-		var forwardedIps = forwardedIpsStr.split(',');
-		ipAddress = forwardedIps[0];
-	}
-	if (!ipAddress) {
-		// Ensure getting client IP address still works in
-		// development environment
-		ipAddress = req.connection.remoteAddress;
-	}
-	return ipAddress;
-};
-
 app.use(function(req, res, next) { // req fixer
-	req.ip = getClientIp(req);
 	req.originalUrl = req.originalUrl.replace(/&?_=\d*/, '');
 	next();
 });
